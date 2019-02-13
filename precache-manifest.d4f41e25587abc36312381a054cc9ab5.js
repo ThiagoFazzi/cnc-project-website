070d59f932235c260548637ba4c6649f57396d9a
ThiagoFazzi <thiago@xtwo.com.br>
2019-02-13 17:11:42 +0100

    Updates

---
+++ precache-manifest.d4f41e25587abc36312381a054cc9ab5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e48c03dc7b11d515090ecaf1283dc498",
+    "url": "/cnc-project-website/static/media/overview.e48c03dc.png"
+  },
+  {
+    "revision": "41234911f3e6684a01e1",
+    "url": "/cnc-project-website/static/css/main.6de3e0b3.chunk.css"
+  },
+  {
+    "revision": "8834cac571c797466c0d",
+    "url": "/cnc-project-website/static/js/1.8834cac5.chunk.js"
+  },
+  {
+    "revision": "84550bfd0dbb1c3e4e45",
+    "url": "/cnc-project-website/static/js/runtime~main.84550bfd.js"
+  },
+  {
+    "revision": "e2d1a37697e5707e6a90e86802418045",
+    "url": "/cnc-project-website/static/media/axis-z-part-ready.e2d1a376.jpeg"
+  },
+  {
+    "revision": "e9b5120890f1c33053e1052bf97ac24c",
+    "url": "/cnc-project-website/static/media/header.e9b51208.png"
+  },
+  {
+    "revision": "41234911f3e6684a01e1",
+    "url": "/cnc-project-website/static/js/main.41234911.chunk.js"
+  },
+  {
+    "revision": "b5c348cb422be10d5a1b5b1aea418d98",
+    "url": "/cnc-project-website/static/media/axis-z-part-assembly.b5c348cb.jpeg"
+  },
+  {
+    "revision": "a7cf06200d6defa79782e60f7ce26823",
+    "url": "/cnc-project-website/static/media/project-header.a7cf0620.jpg"
+  },
+  {
+    "revision": "d5c7dd60690bee01dd5a510fc03c5078",
+    "url": "/cnc-project-website/static/media/mechanical-header.d5c7dd60.jpg"
+  },
+  {
+    "revision": "79087fd252a413ff8648148778a0203d",
+    "url": "/cnc-project-website/static/media/eletronic-header.79087fd2.jpg"
+  },
+  {
+    "revision": "d1c2d792385c824fbe746a2bbd6a5084",
+    "url": "/cnc-project-website/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/cnc-project-website/precache-manifest.d4f41e25587abc36312381a054cc9ab5.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cnc-project-website/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
